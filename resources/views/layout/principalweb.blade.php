4030876bb6295c87e644d74eaa21c4951a9ebdf4
maurowp <mauro.wpg@gmail.com>
2015-09-10 01:37:55 -0430

    Calendarios y arreglos al css

--- resources/views/layout/principalweb.blade.php
+++ resources/views/layout/principalweb.blade.php
@@ -4,20 +4,21 @@
         <title>GAMA | @yield('title','Login')</title>
         <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('assets/css/principalweb.css') }}">
-
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap-datetimepicker.min.css') }}">
         @section('alternalCSS')
 
         @show
 
-        <link rel="stylesheet" href="{{ URL::asset('assets/js/jquery-2.1.4.min.js') }}">
-
+        <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-2.1.4.min.js') }}"> </script>
+        <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"> </script>
+        <script type="text/javascript" src="{{ URL::asset('assets/js/moment.js') }}"> </script>
+        <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap-datetimepicker.min.js') }}"> </script>
         @section('alternalJS')
 
         @show
     </head>
+
     <body>
-
-
         <header>
           <div class="row" style="margin:0px">
             <h1>Holetes Venezuela</h1>
@@ -30,6 +31,33 @@
                 </span>
             </div><!-- /input-group -->
           </div>
+
+          <br />
+          <!-- Calendarios (Botones de busqueda) -->
+          <div class="row">
+              <div class='col-md-2 col-md-offset-4'>
+                  <div class="form-group">
+                      <span>   Entrada  <span class="glyphicon glyphicon-log-in"></span> </span>
+                      <div class='input-group date' id='datetimepicker6'>
+                          <input type='button' class="form-control" id="date1"/>
+                          <span class="input-group-addon">
+                              <span class="glyphicon glyphicon-calendar"></span>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+              <div class='col-md-2'>
+                  <div class="form-group">
+                      <span>  Salida <span class="glyphicon glyphicon-log-out"></span></span>
+                      <div class='input-group date' id='datetimepicker7'>
+                          <input type='button' class="form-control" id="date2"/>
+                          <span class="input-group-addon">
+                              <span class="glyphicon glyphicon-calendar"></span>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </header>
 
         <div class="container">
@@ -37,10 +65,28 @@
             <section class="row">
                 @section('content')
 
-                @show 
+                @show
 
             </section>
 
         </div>
     </body>
+
+    <script type="text/javascript">
+      $(function () {
+          $('#datetimepicker6').datetimepicker({
+              format : "DD-MM-YYYY",
+          });
+          $('#datetimepicker7').datetimepicker({
+              format : "DD-MM -YYYY",
+              useCurrent: false //Important! See issue #1075
+          });
+          $("#datetimepicker6").on("dp.change", function (e) {
+              $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+          });
+          $("#datetimepicker7").on("dp.change", function (e) {
+              $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+          });
+      });
+    </script>
 </html>
