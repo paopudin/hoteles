1298ccf520190da70640a9c60bbf9c24fd4370d6
paolag <pacri14@gmail.com>
2015-09-17 00:12:53 -0430

    cambios

--- resources/views/layout/principalweb.blade.php
+++ resources/views/layout/principalweb.blade.php
@@ -1,92 +1,101 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>GAMA | @yield('title','Login')</title>
-        <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('assets/css/principalweb.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap-datetimepicker.min.css') }}">
-        @section('alternalCSS')
+<head>
+    <title>GAMA | @yield('title','Login')</title>
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/principalweb.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+    @section('alternalCSS')
 
-        @show
+    @show
 
-        <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-2.1.4.min.js') }}"> </script>
-        <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"> </script>
-        <script type="text/javascript" src="{{ URL::asset('assets/js/moment.js') }}"> </script>
-        <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap-datetimepicker.min.js') }}"> </script>
-        @section('alternalJS')
+    <script type="text/javascript" src="{{ URL::asset('assets/js/jquery-2.1.4.min.js') }}"> </script>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"> </script>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/moment.js') }}"> </script>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap-datetimepicker.min.js') }}"> </script>
+    @section('alternalJS')
 
-        @show
-    </head>
+    @show
+</head>
 
-    <body>
-        <header>
-          <div class="row" style="margin:0px">
-            <h1>Holetes Venezuela</h1>
-            <div class="input-group col-md-5">
-                <input type="text" class="form-control" placeholder="Search for...">
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button">
-                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                    </button>
-                </span>
-            </div><!-- /input-group -->
-          </div>
+<body>
+    <header>
+      <div class="container">
+      <div class="row">
+        <h1>Holetes Venezuela</h1>
+        <div class="input-group col-md-5">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </button>
+            </span>
+        </div><!-- /input-group -->
+      </div>
 
-          <br />
-          <!-- Calendarios (Botones de busqueda) -->
-          <div class="row">
-              <div class='col-md-2 col-md-offset-4'>
-                  <div class="form-group">
-                      <span>   Entrada  <span class="glyphicon glyphicon-log-in"></span> </span>
-                      <div class='input-group date' id='datetimepicker6'>
-                          <input type='button' class="form-control" id="date1"/>
-                          <span class="input-group-addon">
-                              <span class="glyphicon glyphicon-calendar"></span>
-                          </span>
-                      </div>
+     <br/>
+      <!-- Calendarios (Botones de busqueda) -->
+        <div class="row">
+            <div class="col-md-2 col-md-offset-3">
+                <div class="form-group">
+                  <span>   Entrada  <span class="glyphicon glyphicon-log-in"></span> </span>
+                  <div class='input-group date' id='datetimepicker6'>
+                      <input type='button' class="form-control" id="date1"/>
+                      <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-calendar"></span>
+                      </span>
                   </div>
-              </div>
-              <div class='col-md-2'>
-                  <div class="form-group">
-                      <span>  Salida <span class="glyphicon glyphicon-log-out"></span></span>
-                      <div class='input-group date' id='datetimepicker7'>
-                          <input type='button' class="form-control" id="date2"/>
-                          <span class="input-group-addon">
-                              <span class="glyphicon glyphicon-calendar"></span>
-                          </span>
-                      </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                  <span>  Salida <span class="glyphicon glyphicon-log-out"></span></span>
+                  <div class='input-group date' id='datetimepicker7'>
+                      <input type='button' class="form-control" id="date2"/>
+                      <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-calendar"></span>
+                      </span>
                   </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+              <br>  
+              <div class="form-group has-feedback"> 
+                  <input type="button" class="form-control" value="Habitacion" />
+                  <i class="glyphicon glyphicon-bed form-control-feedback"></i>
               </div>
-          </div>
-        </header>
+            </div>
+        </div> 
+      </div>
+    </header>
 
-        <div class="container">
+    <div class="container content">
 
-            <section class="row">
-                @section('content')
+        <section class="row">
+            @section('content')
 
-                @show
+            @show
 
-            </section>
+        </section>
 
-        </div>
-    </body>
+    </div>
+</body>
 
-    <script type="text/javascript">
-      $(function () {
-          $('#datetimepicker6').datetimepicker({
-              format : "DD-MM-YYYY",
-          });
-          $('#datetimepicker7').datetimepicker({
-              format : "DD-MM -YYYY",
-              useCurrent: false //Important! See issue #1075
-          });
-          $("#datetimepicker6").on("dp.change", function (e) {
-              $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-          });
-          $("#datetimepicker7").on("dp.change", function (e) {
-              $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-          });
+<script type="text/javascript">
+  $(function () {
+      $('#datetimepicker6').datetimepicker({
+          format : "DD-MM-YYYY",
       });
-    </script>
+      $('#datetimepicker7').datetimepicker({
+          format : "DD-MM -YYYY",
+          useCurrent: false //Important! See issue #1075
+      });
+      $("#datetimepicker6").on("dp.change", function (e) {
+          $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+      });
+      $("#datetimepicker7").on("dp.change", function (e) {
+          $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+      });
+  });
+</script>
 </html>
