ec5c71cde8c30eb312905a97949e99081e0608e9
paolag <pacri14@gmail.com>
2015-09-18 11:38:06 -0430

    left bar

--- resources/views/layout/principalinicioweb.blade.php
+++ resources/views/layout/principalinicioweb.blade.php
@@ -72,9 +72,14 @@
     <div class="container content">
 
         <section class="row">
-            @section('content')
+            <div class="col-md-3 col-sm-3 leftbar" >
+                asd
+            </div>
+            <div class="col-md-9 col-sm-9 rightbar">
+                asd
+            </div>
 
-            @show
+
 
         </section>
 
